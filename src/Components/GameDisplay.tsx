ca4ef4c74aad482b54d731293dac9813e847918b
N1ko1a <ivanovicn98@gmail.com>
2023-10-31 12:04:46 +0100

    Dodao sam da prikazuje igrice prema genres

--- src/Components/GameDisplay.tsx
+++ src/Components/GameDisplay.tsx
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from "react";
 import GameCart from "./GameCart";
 import ReactPaginate from "react-paginate";
 import LoadSkeleton from "./LoadSkeleton";
+import MenuButtons from "./MenuButtons";
 
 function GameDisplay() {
   const [games, setGames] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(0); // Start from the first page
-  const itemsPerPage = 20; // Number of games to display per page
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedGenre, setSelectedGenre] = useState("all"); // Default to "all" genres
+  const itemsPerPage = 20;
 
   useEffect(() => {
     setIsLoading(true);
-    const pageToFetch = currentPage + 1; // Adjust for 1-based page numbering
-    const apiURL = `https://api.rawg.io/api/games?key=4557ebdc3256470e8e4b78f25d277a04&dates=2019-09-01,2023-10-18&page=${pageToFetch}&page_size=${itemsPerPage}&ordering=-popularity`;
+    const pageToFetch = currentPage + 1;
+    const genreFilter = selectedGenre === "all" ? "" : `&genres=${selectedGenre}`;
+    const apiURL = `https://api.rawg.io/api/games?key=4557ebdc3256470e8e4b78f25d277a04&dates=2019-09-01,2023-10-18&page=${pageToFetch}&page_size=${itemsPerPage}&ordering=-popularity${genreFilter}`;
 
     fetch(apiURL)
       .then((res) => res.json())
@@ -24,51 +27,52 @@ function GameDisplay() {
         console.error('Error: ', error);
         setIsLoading(false);
       });
-  }, [currentPage]);
+  }, [currentPage, selectedGenre]);
 
   const handlePageClick = (data) => {
     setCurrentPage(data.selected);
   };
 
+  const handleGenreChange = (genre) => {
+    setSelectedGenre(genre);
+  };
+
   return (
     <div>
-      <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-10 mt-20 mr-20 ml-20 justify-center items-center overflow-hidden">
+      <MenuButtons onGenreChange={handleGenreChange} selectedGenre={selectedGenre} />
+      <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-10 mt-5 mr-20 ml-20 justify-center items-center overflow-hidden">
         {isLoading ? (
           Array.from({ length: itemsPerPage }).map((_, index) => (
-            <LoadSkeleton/>
+            <LoadSkeleton />
           ))
         ) : (
           games.map((game) => (
             <GameCart
               id={game.id}
-
               background={game.background_image}
               name={game.name}
               rating={game.rating}
             />
-
           ))
         )}
       </div>
-            <div className="flex justify-center  items-center">
-
-     <ReactPaginate
-  previousLabel={"Previous"}
-  nextLabel={"Next"}
-  breakLabel={"..."}
-  pageCount={100} // Set to 5 for 5 pages
-  marginPagesDisplayed={2}
-  pageRangeDisplayed={5}
-  onPageChange={handlePageClick}
-  containerClassName={"flex flex-row text-white justify-between items-center w-96 h-10 p-2"} // Apply Tailwind classes to the container
-  subContainerClassName={"pages pagination"}
-  activeClassName={"active"}
-  previousClassName={"hover:text-black ease-in-out duration-300"} // Style the "Previous" button
-  nextClassName={" hover:text-black ease-in-out duration-300"} // Style the "Next" button
-  pageClassName={" hover:text-black ease-in-out duration-300"} // Style the page numbers
-  breakClassName={""} // Style the ellipsis ("...") element
-/>
-
+      <div className="flex justify-center items-center">
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          breakLabel={"..."}
+          pageCount={100}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={"flex flex-row text-white justify-between items-center w-96 h-10 p-2"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+          previousClassName={"hover:text-black ease-in-out duration-300"}
+          nextClassName={"hover:text-black ease-in-out duration-300"}
+          pageClassName={"hover:text-black ease-in-out duration-300"}
+          breakClassName={""}
+        />
       </div>
     </div>
   );
---
+++ src/Components/MenuButtons.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+function MenuButtons({ onGenreChange, selectedGenre }) {
+  const [genres, setGenres] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const apiURL = "https://api.rawg.io/api/genres?key=4557ebdc3256470e8e4b78f25d277a04";
+
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((data) => {
+        setGenres(data.results);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error: ', error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="flex justify-center mt-10">
+      <button
+        className={`text-white hover:text-black ease-in-out duration-300 h-10 ml-2 mr-2 text-sm ${
+          selectedGenre === "all" ? "font-bold" : ""
+        }`}
+        onClick={() => onGenreChange("all")}
+      >
+        All
+      </button>
+      {genres.map((genre) => (
+        <button
+          key={genre.id}
+          className={`text-white hover:text-black ease-in-out duration-300 h-10 ml-2 mr-2 text-sm ${
+            selectedGenre === genre.id ? "font-bold" : ""
+          }`}
+          onClick={() => onGenreChange(genre.id)}
+        >
+          {genre.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default MenuButtons;
+
